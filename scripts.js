8dd426a3ede5d201082a31825c85011ccdd95b3e
Derek <viperspitblade@gmail.com>
2013-12-18 23:47:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -379,6 +379,7 @@ poScript = ({
 			Links[34] = ""+cData.toLowerCase()+" is tht wat u do all day? make fun of po users? i bet uve never gotten laid nd ive fucked many bitchs";
 			Links[35] = ""+cData+" Iam no dumed ass cuz that mean I a Lil bitch ass fack ass trainer suck on my pokeballs bitch";
 			Links[36] = ""+cData.toLowerCase()+" your the faggot who protects barrack obamashit who by the way is a goddamn terrerrest";
+			Links[37] = ""+cData.toUpperCase()+" FUK U NIGGA STOP SO MUTHA FUKN RACIST MAN JUST CUZ IM BLACK DOESNT MEAN U CAN BE A FUKN CUNT";
 			var i = Math.floor(Links.length * Math.random())
 			client.network().sendChanMessage(channel, Links[i]);
 			return;
